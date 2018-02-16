a9c2d00dd68133cfe76540338567cbb0181fa07f
Ronak <ronak@mutterfly.in>
2018-02-16 18:34:51 +0530

    episode activity started

--- app/src/main/java/com/example/mf/movielibrary/activities/episodescreen/EpisodeActivity.kt
+++ app/src/main/java/com/example/mf/movielibrary/activities/episodescreen/EpisodeActivity.kt
@@ -1,9 +1,15 @@
 package com.example.mf.movielibrary.activities.episodescreen
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 import com.example.mf.movielibrary.R
 import com.example.mf.movielibrary.base.BaseActivity
+import com.example.mf.movielibrary.models.seasonmodels.Episode
+import files.PARCELABLE_OBJECT
+import files.backdropUrl
+import files.getDateWithCustomFormat
+import files.loadImage
+import kotlinx.android.synthetic.main.activity_episode.*
 
 class EpisodeActivity : BaseActivity<EpisodeActivityContract.EpisodeView, EpisodeActivityPresenter>() {
 
@@ -12,5 +18,14 @@ class EpisodeActivity : BaseActivity<EpisodeActivityContract.EpisodeView, Episod
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_episode)
+
+        val episodeModel = intent.getParcelableExtra(PARCELABLE_OBJECT) as Episode
+        initToolbar(toolbar as Toolbar, true,"")
+
+        episodeBackdropImage.loadImage(backdropUrl + episodeModel.stillPath)
+        episodeName.text = episodeModel.episodeName
+        episodeOverview.text = episodeModel.episodeOverview
+        episodeAirDate.text = getDateWithCustomFormat(episodeModel.episodeAirDate)
+        episodeVoteAverage.text = episodeModel.voteAverage.toString()
     }
 }
