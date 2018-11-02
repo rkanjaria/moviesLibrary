229be4129c0cd9d967b657514d28980b93c3518a
Ronak <ronak@mutterfly.in>
2018-11-02 18:29:31 +0530

    changes done in room database files

--- app/src/main/java/com/example/mf/movielibrary/database/AppDatabase.kt
+++ app/src/main/java/com/example/mf/movielibrary/database/AppDatabase.kt
@@ -7,7 +7,7 @@ import com.example.mf.movielibrary.database.daos.GenreDao
 import com.example.mf.movielibrary.database.entities.FavouriteMovie
 import com.example.mf.movielibrary.database.entities.Genre
 
-@Database(entities = [(Genre::class), (FavouriteMovie::class)], version = 1)
+@Database(entities = [Genre::class, FavouriteMovie::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun genreDao(): GenreDao
--- app/src/main/java/files/ExtensionFunctions.kt
+++ app/src/main/java/files/ExtensionFunctions.kt
@@ -6,7 +6,6 @@ import android.content.Context
 import android.support.annotation.DrawableRes
 import android.support.annotation.LayoutRes
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
@@ -21,9 +20,8 @@ import com.example.mf.movielibrary.database.AppDatabase
  * Created by RK on 23-12-2017.
  */
 
-fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
-    return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
-}
+fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false) =
+        LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
 
 fun ImageView.loadImage(url: String, placeholder: Int = R.color.darkGrey, centerCrop: Boolean = false) {
 
@@ -63,7 +61,6 @@ fun ImageView.loadCircularImage(url: Any) {
             .into(this)
 }
 
-
 fun ImageView.loadDrawableImage(@DrawableRes url: Int, placeholder: Int = R.color.darkGrey) {
     Glide.with(context)
             .load(url)
@@ -93,94 +90,3 @@ fun TextView.expandOrCollapseTextView() {
     animation.setDuration(250)
             .start()
 }
-
-/*fun LinearLayout.asTagsLayout(tagsList: List<String>) {
-
-    val px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8f, context.displayMetrics).toInt()
-    var previousTagWidth = 0
-    var previousTagHeight = 0
-    var left = 0
-    var top = 0
-
-    val displayMetrices = DisplayMetrics()
-    (context as Activity).windowManager.defaultDisplay.getMetrics(displayMetrices)
-    val screenWidth = displayMetrices.widthPixels
-
-    val params = RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-    val tagLayout = RelativeLayout(context)
-    tagLayout.setPadding(px, px * 2, px, px * 2)
-    tagLayout.layoutParams = params
-
-    tagsList.forEach {
-        val tagView = TextView(context)
-        tagView.setPadding(px * 2, px, px * 2, px)
-        tagView.text = tagsList.get(tagsList.indexOf(it))
-        tagView.background = ContextCompat.getDrawable(context, R.drawable.tag_background_drawable)
-        tagView.setTextColor(ContextCompat.getColor(context, R.color.colorAccent))
-        tagView.typeface = ResourcesCompat.getFont(context, R.font.noto_sans_regular)
-        tagView.textSize = 12f
-        tagView.maxLines = 1
-        tagView.gravity = Gravity.CENTER
-        tagView.ellipsize = TextUtils.TruncateAt.END
-
-        val tagViewParams = RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        left = left + previousTagWidth
-
-        if (((screenWidth - px * 2) - left) < tagView.measureTextViewWidth()) {
-            top = top + previousTagHeight
-            left = 0
-        }
-
-        tagViewParams.setMargins(left, top, 0, 0)
-        tagView.layoutParams = tagViewParams
-        tagLayout.addView(tagView)
-
-        previousTagWidth = tagView.measureTextViewWidth() + px
-        previousTagHeight = tagView.measureTextViewHeight() + px
-    }
-
-    this.addView(tagLayout)
-}
-
-fun TextView.measureTextViewWidth(): Int {
-    this.measure(0, 0)
-    return this.measuredWidth
-}
-
-fun TextView.measureTextViewHeight(): Int {
-    this.measure(0, 0)
-    return this.measuredHeight
-}*/
-
-/*
-fun View.findSelectedTag(context: Context) : String {
-
-    var selectedText = ""
-
-    try {
-        if (this is ViewGroup) {
-            val viewGroup = this
-            for (i in 0 until viewGroup.childCount) {
-                val child = viewGroup.getChildAt(i)
-                child.findSelectedTag(context)
-            }
-        }else if(this is TextView){
-
-            this.setOnClickListener {
-
-                if(this.text.equals()){
-                    this.background = ContextCompat.getDrawable(context, R.drawable.tag_background_colored_drawable)
-                    this.setTextColor(ContextCompat.getColor(context, R.color.colorPrimaryDark))
-                    selectedText = this.text.toString()
-                }else{
-                    this.background = ContextCompat.getDrawable(context, R.drawable.tag_background_drawable)
-                    this.setTextColor(ContextCompat.getColor(context, R.color.colorAccent))
-                }
-            }
-        }
-
-    } catch (e: Throwable) {
-        e.printStackTrace()
-    }
-    return selectedText
-}*/
